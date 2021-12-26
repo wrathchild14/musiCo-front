c90484e02c2a14644b12289683779df269e9b06b
wrath <jovanprodanov123@gmail.com>
2021-12-27 00:17:48 +0100

    Sign-in basic

--- musico/src/router/index.js
+++ musico/src/router/index.js
@@ -4,8 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
@@ -16,8 +15,18 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/sign-up',
+    name: 'Sign-up',
+    component: () => import('../views/Sign-up.vue') // TODO
+  },
+  {
+    path: '/sign-in',
+    name: 'Sign-in',
+    component: () => import('../views/Sign-in.vue') // TODO
+  },
 ]
 
 const router = new VueRouter({
--- musico/src/views/Home.vue
+++ musico/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>HELLO</h1>
+  <v-main>
+    <h1>HELLO</h1>
+    <router-link to="/sign-in">SIGN IN</router-link>
+  </v-main>
 </template>
 
 <script>
@@ -12,7 +15,7 @@ export default {
 
   created() {
     // Axios for already created user with a token
-    const access_token = 'ae8466b307125e94db84ca3e1394da9ca671a29c';
+    const access_token = "ae8466b307125e94db84ca3e1394da9ca671a29c";
 
     getAPI
       .get("/user/", {
